e1f262d22ff24d8453df148c4d81762b9ab34230
tafayzul <107237505+tafayzul@users.noreply.github.com>
2026-01-19 23:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter method for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound message
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    // Call the parent constructor
+    super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    // Call the parent constructor
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
